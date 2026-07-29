62185b6b83674dfab94b08b4e4929feef2cd8548
jahphetB <japhetlakew098@gmail.com>
2026-07-29 17:10:22 -0600

    remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...extraItems) {
+    let newArray = [];
+    for (let index = 0; index < array.length; index++) {
+        if (!extraItems.includes(array[index])){
+            newArray.push(array[index]);
+        }
+    }
+    return newArray; 
 };
 
 // Do not edit below this line
