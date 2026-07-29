a9f446fbcf5fd5caa845ec2c2617fd1f1a1f5781
jahphetB <japhetlakew098@gmail.com>
2026-07-29 16:32:57 -0600

    done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    let string = "";
+    for (let index = 0; index < number; index++) {
+        string += word
+    }
+    return string;
 };
 
 // Do not edit below this line
