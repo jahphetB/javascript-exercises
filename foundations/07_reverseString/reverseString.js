7256059e3a1a7d5891d5ac911832c91f8c36778f
jahphetB <japhetlakew098@gmail.com>
2026-07-29 16:42:31 -0600

    done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    for (let index = string.length - 1; index >= 0; index--) {
+        const element = string[index];
+        reversed += element;
+    }
+    return reversed; 
 };
 
 // Do not edit below this line
