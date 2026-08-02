86e5c68613ee66197148fae15f1bdad433fdd023
jahphetB <japhetlakew098@gmail.com>
2026-08-01 20:55:30 -0600

    sumAll between numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0) {
+        let sum = 0;
+        if  (num1 < num2) {
+            while (num1 <= num2) {
+                sum += num1
+                num1 ++;
+            }
+            
+        } else if (num1 > num2) {
+            while (num1 >= num2) {
+                sum += num1
+                num1 --;
+            }
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
