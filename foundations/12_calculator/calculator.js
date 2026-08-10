2d8c676ced65354088c3d6a39b9256376c3da06e
jahphetB <japhetlakew098@gmail.com>
2026-08-09 20:15:18 -0600

    added sub and sum functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,12 +2,16 @@ const add = function(x, y) {
   return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+  let total = 0;
+	arg.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
 const multiply = function() {
