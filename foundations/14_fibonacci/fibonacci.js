d789664e258c8781be88ce010e7244189b539087
jahphetB <japhetlakew098@gmail.com>
2026-08-10 11:17:52 -0600

    fibonacci completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = Number(n);
+    let fib = [0, 1];
+    if (num === 0){
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= num; i++) {
+            fib.push(fib[i - 1] + fib[i - 2])
+        }
+        return fib[fib.length -1];
+    }
 
 };
 
